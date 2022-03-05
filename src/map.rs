9cc51e64f6ee07616f67fa340f0139b44b62b191
Chris <cmmooney852@gmail.com>
2022-03-05 16:41:07 +1000

    implmented basic tunnels

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,6 @@
 use super::Rect;
 use bracket_lib::prelude::*;
+use std::cmp::{max, min};
 
 #[derive(PartialEq, Clone, Copy)]
 pub enum TileType {
@@ -43,6 +44,24 @@ pub fn new_map_test() -> Vec<TileType> {
     map
 }
 
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     let mut map = vec![TileType::Wall; 80 * 50];
 
@@ -51,6 +70,8 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
 
     apply_room_to_map(&room1, &mut map);
     apply_room_to_map(&room2, &mut map);
+
+    apply_horizontal_tunnel(&mut map, 25, 40, 23);
 
     map
 }
